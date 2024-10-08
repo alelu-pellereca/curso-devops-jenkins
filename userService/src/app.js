0d1e5ac930dde433b99d1d0e2339e9be84b410d7
alelu-pellereca <alejandrapellegrini@gmail.com>
2024-10-08 21:06:12 +0200

    nuevisimo3445

--- userService/src/app.js
+++ userService/src/app.js
@@ -5,7 +5,7 @@ app.use(express.json());
 
 // Ruta básica
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello World probando!' });
+    res.status(200).json({ message: 'Hello World probando 3001!' });
 });
 
 // Ruta de ejemplo para pruebas
