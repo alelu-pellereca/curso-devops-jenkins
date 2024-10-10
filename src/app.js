5e3efae3ed0a98fdb50791c0f00f08bd46695471
alelu-pellereca <alejandrapellegrini@gmail.com>
2024-10-10 16:31:30 +0200

    tocommit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ app.use(express.json());
 
 // Ruta básica
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello World nuevo!' });
+    res.status(200).json({ message: 'Hello World dockernuevo!' });
 });
 
 // Ruta de ejemplo para pruebas
